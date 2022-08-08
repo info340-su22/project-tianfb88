3c94bc300e2e34b5f1e0029002a744f8dd98d64b
Sylviaviaviavia <103002698+Sylviaviaviavia@users.noreply.github.com>
2022-08-08 21:35:11 +0800

    Add files via upload

--- react_file/src/App.js
+++ react_file/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <body>
+            <main>
+                <div class="container">
+                    <section class="sect1">
+                    <h3 class="question">Distance to UW Campus</h3>
+                    <div class="form-check">
+                    </div>
+                    <h3 class="question">Monthly Rent</h3>
+                    <div class="form-check">
+                    </div>
+                    <h3 class="question">Floor Plan</h3>
+                    <div class="form-check">
+                    </div>
+                    </section>
+                    <section class="sect2">
+                        <div class="new-container">
+                            <div class="card" style="width: 20rem;">
+                            </div>
+                        </div>
+                    </section>
+                    <section class="sect3">
+                        <ul class="info">
+                        </ul>
+                    </section>
+                </div>
+            </main>
+            <div class="footer-basic">
+                <footer>
+                </footer>
+            </div>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+        </body>
+    )
 }
-
-export default App;
