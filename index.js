bde874b11eeb13ed8a9bf9bcd87d2f9046dc95af
haonanzzz <hz96@uw.edu>
2022-08-08 21:31:28 -0700

    appointment app

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import '.css/style.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import {Appointment} from './appointment.js';
+import {CrowdSource} from './crowdsource';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
+          <Route path="appointment" element={<Appointment />}>
+            <Route path="crowdsource" element={<CrowdSource />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     <App />
   </React.StrictMode>  
+
 );
