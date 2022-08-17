c63175eb9ed345794ce6a91cb63f474a9c5a4684
Sylviaviaviavia <103002698+Sylviaviaviavia@users.noreply.github.com>
2022-08-18 00:21:06 +0800

    Add files via upload
    revise index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Appointment from './components/appointment';
-import CrowdSource from './components/crowdsource';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="home" element={<App />} />
-        <Route index element={<App />} />
-        <Route path="appointment" element={<Appointment />} />
-        <Route path="crowdsource" element={<CrowdSource />} />
-      </Routes>
-
-      <App />
-    </BrowserRouter>
-    
-  </React.StrictMode>  
-
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
