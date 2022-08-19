943694f8a26e0cb8f84c503ad1f218f5c879ba56
Sylviaviaviavia <103002698+Sylviaviaviavia@users.noreply.github.com>
2022-08-19 15:27:13 +0800

    Add files via upload
    renew crowdsource, add react, save user input into data file

--- src/components/crowdsource.js
+++ src/components/crowdsource.js
@@ -1,119 +1,124 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { dataList } from "../data/data";
 
-export default function CrowdSource(props) {
-    const [name, setName] = useState('');
-    const [loc, setLoc] = useState(null);
-    const [floorPlan, setFloorPlan] = useState('');
-    const[rating, setRating] = useState(null);
-    const [comment, setComment] = useState('');
-    const [detail, setDetail] = useState({"key":null, "name":'', "floorplan":'',"rate":null,"comment":[], "times":''});
+const $ = window.$;
+function Crowd(props) {
+  const [submitForm, setSubmitForm] = useState({
+    key: 1,
+    rate: 0,
+    comment: "",
+  });
 
-    const handleName = (event) => {
-        let lowerCaseInput = event.target.value.toLowerCase();
-        setName(lowerCaseInput);
+  const onSubmit = () => {
+    const item = props.data.filter(
+      (item) => item.key === parseInt(submitForm.key)
+    )[0];
+    const { rateList } = item;
+    if (rateList) {
+      rateList.push(parseInt(submitForm.rate));
+      let sumRate = 0;
+      rateList.forEach((item) => {
+        sumRate += item;
+      });
+      item.rate = (sumRate / rateList.length).toFixed(1);
+    } else {
+      item.rateList = [parseInt(submitForm.rate)];
+      item.rate = item.rateList[0];
     }
+    item.times = item.rateList.length;
+    item.comment.push(submitForm.comment);
+    alert("Form has been submitted!"); 
+    $(document.getElementById("reset")).click()
+    return false;
+  };
 
-    const handleLoc = (event) => {
-        setRating(event.target.value)
-    }
-
-    const handleFP = (event) => {
-        let updateFP = [...floorPlan];
-        if(event.target.checked) {
-            updateFP = [...floorPlan, event.target.value];
-            let updateFP = updateFP.toString();
-        }
-        setFloorPlan(updateFP);
-    }
-    const handleRate = (event) => {
-        setRating(event.target.value)
-    }
-
-    const handleCom = (event) => {
-        setComment(event.target.value);
-    }
-
-    const handleSend = (event) => {
-        let exist = null;
-        event.preventDefault();
-        console.log('Your rating has been sent!');
-        if(props.data.name.includes(name)){
-            exist = props.data.filter((apt) => apt.name.toLowerCase() === name);
-            exist.times = exist.times+1;
-            exist.rate = round((exist.rate + rating)/times, 1);
-            exist.comment = [...exist.comment, comment];
-        } else {
-            setDetail({"key":props.data.length+1, "name": name, "floorplan": floorPlan, "rate": rating, "comment":comment, "times":1});
-            props.data = [...props.data, detail];
-        }
-        setName('');
-        setLoc(null);
-        setFloorPlan([]);
-        setRating(null);
-    }
-    
-    return(
-                <div className='crowd'>
-                <div className="main-block">
-                    <form className="rate" action="/">
-                    <h2 className="resident">Residents Rating</h2>
-                    <h3>Apartment Name</h3>
-                    <input type="text" onChange={handleName} />
-                    <div>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <th className="second-col"></th>
-                                    <th>On Campus</th>
-                                    <th>Off Campus</th>
-                                </tr>
-                                <tr>
-                                    <td className="second-col">Location</td>
-                                    <td><input type="radio" value="On-campus Apartment" name="location" checked={loc === 'On-campus Apartment'} onChange={handleLoc} /></td>
-                                    <td><input type="radio" value="Off-campus Apartment" name="location" checked={loc === 'Off-campus Apartment'} onChange={handleLoc} /></td>
-                                </tr>
-                                <tr>
-                                    <th className="second-col"></th>
-                                    <th>Studio</th>
-                                    <th>1B</th>
-                                    <th>2B</th>
-                                    <th>3B</th>
-                                    <th>4B</th>
-                                </tr>
-                                <tr>
-                                    <td className="second-col">Floor Plans</td>
-                                    <td><input type="checkbox" value="Studio" name="floor" onChange={handleFP} /></td>
-                                    <td><input type="checkbox" value="1B" name="floor" onChange={handleFP} /></td>
-                                    <td><input type="checkbox" value="2B" name="floor" onChange={handleFP} /></td>
-                                    <td><input type="checkbox" value="3B" name="floor" onChange={handleFP} /></td>
-                                    <td><input type="checkbox" value="4B" name="floor" onChange={handleFP} /></td>
-                                </tr>
-                                <tr>
-                                    <th className="first-col"></th>
-                                    <th>1</th>
-                                    <th>2</th>
-                                    <th>3</th>
-                                    <th>4</th>
-                                    <th>5</th>
-                                </tr>
-                                <tr>
-                                    <td className="first-col">Your Rating</td>
-                                    <td><input type="radio" value="1" name="distance" checked={rating === '1'} onChange={handleRate} /></td>
-                                    <td><input type="radio" value="2" name="distance" checked={rating === '2'} onChange={handleRate} /></td>
-                                    <td><input type="radio" value="3" name="distance" checked={rating === '3'} onChange={handleRate} /></td>
-                                    <td><input type="radio" value="4" name="distance" checked={rating === '4'} onChange={handleRate} /></td>
-                                    <td><input type="radio" value="5" name="distance" checked={rating === '5'} onChange={handleRate} /></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <p className="comments">Any further comment</p>
-                    <textarea rows="5" value={comment} onChange={handleCom} ></textarea>
-                    <div className="btn-block">
-                        <button className="sub" type="submit" href="/" onSubmit={handleSend}>Send</button>
-                    </div>
-                    </form>
-                </div>
+  return (
+    <>
+      <main>
+        <div className="main-block">
+          <form className="rate">
+            <h2 className="resident">Residents Rating</h2>
+            <h3>Apartment Name</h3>
+            <select
+              className="form-control"
+              onChange={(event) => {
+                setSubmitForm({
+                  ...submitForm,
+                  key: event.target.value,
+                });
+              }}
+            >
+              {props.data.map((item) => (
+                <option key={item.key} value={item.key}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <th className="first-col"></th>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>4</th>
+                    <th>5</th>
+                  </tr>
+                  <tr>
+                    <td className="second-col">Rating</td>
+                    {(() => {
+                      const res = [];
+                      for (let index = 0; index < 5; index++) {
+                        res.push(
+                          <td key={index}>
+                            <input
+                              type="radio"
+                              value={index + 1}
+                              name="distance"
+                              onChange={(event) => {
+                                setSubmitForm({
+                                  ...submitForm,
+                                  rate: event.target.value,
+                                });
+                              }}
+                            />
+                          </td>
+                        );
+                      }
+                      return res;
+                    })()}
+                  </tr>
+                </tbody>
+              </table>
             </div>
-    )
+            <p className="comments">Any further comment</p>
+            <textarea
+              rows="5"
+              required
+              onInput={(event) => {
+                setSubmitForm({
+                  ...submitForm,
+                  comment: event.target.value,
+                });
+              }}
+            ></textarea>
+            <div className="btn-block">
+              <button className="sub" type="button" onClick={onSubmit}>
+                Send
+              </button>
+              <button
+                className="sub"
+                type="reset"
+                id="reset"
+                style={{ display: "none" }}
+              ></button>
+            </div>
+          </form>
+        </div>
+      </main>
+    </>
+  );
 }
+
+export default Crowd;
